65cba2fa2898de172699be2a0b2fc59e3f0d7c39
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-09-14 12:16:37 +0200

    exo 2

---
+++ easy/longest_string_without_repeating_characters.py
@@ -0,0 +1,31 @@
+
+
+class Solution():
+    def __init__(self):
+        pass
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charSet = set()
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                maxLength = max(maxLength, right - left + 1)
+
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
+        return maxLength
+                
+
+        
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.lengthOfLongestSubstring("pwwkew"))
+        
