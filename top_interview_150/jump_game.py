4419e0df1ab9696f042cdad7a00a49f00cc28334
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-11 19:55:28 +0200

    new challenge

--- top_interview_150/jump_game.py
+++ top_interview_150/jump_game.py
@@ -2,10 +2,21 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        last_index = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):
+            print('i: ', i)
+            print('nums[i]: ', nums[i])
+            print('last_index: ', last_index)
+
+            if i + nums[i] >= last_index:
+                last_index = i
+                print('last_index: ', last_index)
+        return last_index == 0
 
 
 if __name__ == '__main__':
     sol = Solution()
     nums = [2,3,1,1,4]
+    print(sol.canJump(nums))
+    nums = [3,2,1,0,4]
     print(sol.canJump(nums))
