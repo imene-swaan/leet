8410829a3a6a128b4ea8deecf85f92aa29fb800a
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-15 15:20:49 +0200

    h index

---
+++ top_interview_150/h-index.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    citations = [3,0,6,1,5]
+    print(sol.hIndex(citations))
